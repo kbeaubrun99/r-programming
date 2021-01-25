9891a19e0a26ea9c44c0664c29fcd36b1d46dfd7
kbeaubrun99 <77594666+kbeaubrun99@users.noreply.github.com>
2021-01-25 04:55:35 -0500

    Add files via upload

---
+++ Mod2.R
@@ -0,0 +1,9 @@
+#Code from Mod 2 assignment
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(assignment2) { return(sum(assignment)/length(someData)) }
+#This code won't run properly because it's missing input variables
+#Will add assignment and someData variables so it will run properly
+assignment <- c(17, 19, 15, 22, 27, 18, 20, 18, 18, 23, 21, 23)
+someData <- c(17, 19, 15, 22, 27, 18, 20, 18, 18, 23, 21, 23)
+#Now that we added the two vectors the function should execute correctly!
+myMean(assignment2)
